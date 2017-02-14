0af38c85eb6dde178612ecfa202e5912b9397ad8
Liujx <zhoumo@sayweb.top>
2017-02-14 22:35:18 +0800

    导航完成

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('#dowebok').fullpage({
+        sectionsColor: ['#F5F5F5', '#4BBFC3', '#7BAABE', '#f90', "#0089A7", "#2EA9DF"],
+        'navigation': true,
+        navigationTooltips: ['首页', '关于我', '专业技能', '我的经历', '我的作品', '联系我'],
+        loopBottom: true,
+        resize: true
+    });
+});
