76ab634d2bbbd83c37c300db47118caa16f5f1f2
Liujx <zhoumo@sayweb.top>
2017-02-18 22:43:09 +0800

    page one

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {
     $('#dowebok').fullpage({
-        sectionsColor: ['#F5F5F5', '#4BBFC3', '#7BAABE', '#f90', "#0089A7", "#2EA9DF"],
-        'navigation': true,
+        sectionsColor: ['#0B3042', '#356869', '#75AC8F', '#A1CFA1', "#B7CF91", "#F4F4F6"],
+        navigation: true,
         navigationTooltips: ['首页', '关于我', '专业技能', '我的经历', '我的作品', '联系我'],
         loopBottom: true,
         resize: true
